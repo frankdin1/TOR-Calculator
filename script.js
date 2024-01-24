70a1e355d95d049f4233ada65b180d5b7a6aa193
frankdin <frankdin@yahoo.com>
2024-01-23 19:12:53 -0500

    Created skeleton html for calculator.

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+-calculator should have 4 operators- add, subtract, divide, multiply
+-create a function 'operate' that takes an operation (+,/,*,-), two numbers and performs the operation
+-add a display to the calculator
+-create functions that populate the display when you click number buttons
+-store the the 'display value' in a variable
+-users should be able to string operations together and operations should work in proper order
+-calculator should evaluate more than a single pair of numbers at a time.
+-round answers with long decimals
+-pressing clear should acutally reset the data
+-display error messages of users try to divide by 0
+
+Extra credit
+-add a '.' button so users caninput decimals
+-users should not type more than one decimal
+-add a backspace button so users can undo 
+-add keyboard support in case you get trouble from keys such as '/'. read documentation for 'event.preventDefault' tosolve this problem.
+*/
+
